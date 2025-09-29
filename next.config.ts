f308ba8e0342c1cb6b8ba5b492d03b2b599f9b8b
Никита <cryptonpochta@gmail.com>
2025-09-29 18:43:27 +0300

    News: use remote source images with Next Image; add more metadata and fallbacks; allow domains in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "www.google.com", pathname: "/s2/favicons/**" },
+      { protocol: "https", hostname: "coindesk.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.coindesk.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.binance.com", pathname: "/**" },
+      { protocol: "https", hostname: "binance.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.rbc.ru", pathname: "/**" },
+      { protocol: "https", hostname: "rbc.ru", pathname: "/**" },
+      { protocol: "https", hostname: "holder.io", pathname: "/**" },
+      { protocol: "https", hostname: "www.holder.io", pathname: "/**" },
+      { protocol: "https", hostname: "nft.ru", pathname: "/**" },
+      { protocol: "https", hostname: "www.nft.ru", pathname: "/**" },
+    ],
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
